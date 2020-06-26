f0d7aff168e5376dc6422926f915d21834a04bda
UmmeSalma2614 <67107822+UmmeSalma2614@users.noreply.github.com>
2020-06-26 22:26:45 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,5 @@
+# Write a Python program to perform Deletion of a character
+
+s1="apple"
+print(s1)
+print(s1[1:])
---
+++ q4.py
@@ -0,0 +1,5 @@
+# Write a program to print every character of a string entered by user in a new line using loop
+
+s1="mango"
+for i in s1:
+	print(i)
